3370de5078f16354c7068551652fdfe940e9cc9d
nxrskyaa <kyosiwijaya@gmail.com>
2026-05-15 10:48:54 +0700

    chore: stabilize tsconfig project references for local build

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,11 +3,11 @@ import Navbar from './Navbar'
 
 export default function Layout() {
   return (
-    <div className="min-h-screen" style={{ backgroundColor: '#0A0A0F', color: '#F0F0F5' }}>
+    <div className="min-h-screen bg-[hsl(var(--background))] text-[hsl(var(--foreground))] transition-colors duration-300">
       <Navbar />
-      <div className="pt-24">
+      <main className="mx-auto w-full max-w-6xl px-4 pb-10 pt-24 sm:px-6">
         <Outlet />
-      </div>
+      </main>
     </div>
   )
 }
